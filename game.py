35e2ac2f5b4b47a243dca20683cb389e234c958c
jbrown400 <justin.brown400@gmail.com>
2017-12-18 18:56:22 -0600

    Looping through each player's turn

--- game.py
+++ game.py
@@ -13,23 +13,35 @@ class Game():
 
     def turn(self):
         # self.board.print_board(self.players["white"], self.players["black"])
-        self.board.print_board(self.players)
+        
+        # Loop until someone loses
+        while(self.players["white"].lost != True or self.players["black"].lost != True):
+            self.board.print_board(self.players)
+            
+            # Get user input from the white player
+            print("White move: ", end="")
+            self.white_move = self.validate_input()
+            print(self.white_move)
+            # while(not self.validate_input(input("White move: "))):
+            #    pass
 
-        # Get user input from the white player
-        while(not self.validate_input(input("White move: "))):
-            pass
-        # Get user input from the black player
+            if(self.players["white"].lost == True):
+                continue
 
-    def validate_input(self, move):
-        print("Validating move: " + move)
-        if(len(move) != 2):
-            print("Invalid length for a move")
-            return False
-        elif(move[0] not in self.valid_letters):
-            print("Invalid letter for move")
-            return False
-        elif(move[1] not in self.valid_numbers):
-            print("Invalid number for move")
-            return False
-        else:
-            return True
+            # Get user input from the black player
+            print("Black move: ", end="")
+            self.black_move = self.validate_input()
+            print(self.black_move)
+
+    def validate_input(self):
+        while(True):
+            move = input()
+            if(len(move) != 2):
+                print("Invalid length for a move")
+            elif(move[0] not in self.valid_letters):
+                print("Invalid letter for move")
+            elif(move[1] not in self.valid_numbers):
+                print("Invalid number for move")
+            else:
+                return move
+            print("Try again: ", end="")
